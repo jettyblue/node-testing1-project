6ce008f230232f93e36bf4888f7d84042986d58c
unknown <lindsay.kelly15@gmail.com>
2022-03-30 16:15:32 -0400

    wip

--- index.test.js
+++ index.test.js
@@ -1,36 +1,40 @@
-const utils = require('./index')
+const utils = require('./index');
 
 describe('[Exercise 1] trimProperties', () => {
   test('[1] returns an object with the properties trimmed', () => {
     // EXAMPLE
-    const input = { foo: '  foo ', bar: 'bar ', baz: ' baz' }
-    const expected = { foo: 'foo', bar: 'bar', baz: 'baz' }
-    const actual = utils.trimProperties(input)
-    expect(actual).toEqual(expected)
+    const input = { foo: '  foo ', bar: 'bar ', baz: ' baz' };
+    const expected = { foo: 'foo', bar: 'bar', baz: 'baz' };
+    const actual = utils.trimProperties(input);
+    expect(actual).toEqual(expected);
   })
   test('[2] returns a copy, leaving the original object intact', () => {
-    const input = { foo: '  foo ', bar: 'bar ', baz: ' baz' }
-    utils.trimProperties(input)
-    expect(input).toEqual({ foo: '  foo ', bar: 'bar ', baz: ' baz' })
+    const input = { foo: '  foo ', bar: 'bar ', baz: ' baz' };
+    utils.trimProperties(input);
+    expect(input).toEqual({ foo: '  foo ', bar: 'bar ', baz: ' baz' });
   })
 })
 
 describe('[Exercise 2] trimPropertiesMutation', () => {
   test('[3] returns an object with the properties trimmed', () => {
-    const input = { foo: '  foo ', bar: 'bar ', baz: ' baz' }
-    const expected = { foo: 'foo', bar: 'bar', baz: 'baz' }
-    const actual = utils.trimProperties(input)
-    expect(actual).toEqual(expected)
+    const input = { foo: '  foo ', bar: 'bar ', baz: ' baz' };
+    const expected = { foo: 'foo', bar: 'bar', baz: 'baz' };
+    const actual = utils.trimProperties(input);
+    expect(actual).toEqual(expected);
   })
   test('[4] the object returned is the exact same one we passed in', () => {
-    const input = { foo: '  foo ', bar: 'bar ', baz: ' baz' }
-    const actual = utils.trimPropertiesMutation(input)
-    expect(actual).toBe(input)
+    const input = { foo: '  foo ', bar: 'bar ', baz: ' baz' };
+    const actual = utils.trimPropertiesMutation(input);
+    expect(actual).toBe(input);
   })
 })
 
 describe('[Exercise 3] findLargestInteger', () => {
-  // test('[5] returns the largest number in an array of objects { integer: 2 }', () => {})
+  test('[5] returns the largest number in an array of objects { integer: 2 }', () => {
+    const input = [{ integer: 1 }, { integer: 100 }, { integer: 25 }];
+    const output = utils.findLargestInteger(input);
+    expect(output).toEqual(100);
+  })
 })
 
 describe('[Exercise 4] Counter', () => {
@@ -38,9 +42,24 @@ describe('[Exercise 4] Counter', () => {
   beforeEach(() => {
     counter = new utils.Counter(3) // each test must start with a fresh couter
   })
-  // test('[6] the FIRST CALL of counter.countDown returns the initial count', () => {})
-  // test('[7] the SECOND CALL of counter.countDown returns the initial count minus one', () => {})
-  // test('[8] the count eventually reaches zero but does not go below zero', () => {})
+  test('[6] the FIRST CALL of counter.countDown returns the initial count', () => {
+    counter.countDown();
+    expect(counter.initialNumber).toBe(3);
+  })
+  test('[7] the SECOND CALL of counter.countDown returns the initial count minus one', () => {
+    counter.countDown();
+    counter.countDown();
+    expect(counter.initialNumber).toBe(2);
+  })
+  test('[8] the count eventually reaches zero but does not go below zero', () => {
+    counter.countDown();
+    counter.countDown();
+    counter.countDown();
+    counter.countDown();
+    counter.countDown();
+    counter.countDown();
+    expect(counter.initialNumber).toBe(0);
+  })
 })
 
 describe('[Exercise 5] Seasons', () => {
@@ -48,12 +67,43 @@ describe('[Exercise 5] Seasons', () => {
   beforeEach(() => {
     seasons = new utils.Seasons() // each test must start with fresh seasons
   })
-  // test('[9] the FIRST call of seasons.next returns "summer"', () => {})
-  // test('[10] the SECOND call of seasons.next returns "fall"', () => {})
-  // test('[11] the THIRD call of seasons.next returns "winter"', () => {})
-  // test('[12] the FOURTH call of seasons.next returns "spring"', () => {})
-  // test('[13] the FIFTH call of seasons.next returns again "summer"', () => {})
-  // test('[14] the 40th call of seasons.next returns "spring"', () => {})
+  test('[9] the FIRST call of seasons.next returns "summer"', () => {
+    const output = seasons.next();
+    expect(output).toBe('summer');
+  })
+  test('[10] the SECOND call of seasons.next returns "fall"', () => {
+    seasons.next();
+    const output = seasons.next();
+    expect(output).toBe('fall');
+  })
+  test('[11] the THIRD call of seasons.next returns "winter"', () => {
+    seasons.next();
+    seasons.next();
+    const output = seasons.next();
+    expect(output).toBe('winter');
+  })
+  test('[12] the FOURTH call of seasons.next returns "spring"', () => {
+    seasons.next();
+    seasons.next();
+    seasons.next();
+    const output = seasons.next();
+    expect(output).toBe('spring');
+  })
+  test('[13] the FIFTH call of seasons.next returns again "summer"', () => {
+    seasons.next();
+    seasons.next();
+    seasons.next();
+    seasons.next();
+    const output = seasons.next();
+    expect(output).toBe('summer');
+  })
+  test('[14] the 40th call of seasons.next returns "spring"', () => {
+    for(let i = 0; i < 39; i++) {
+      seasons.next()
+    }
+    const output = seasons.next();
+    expect(output).toBe('spring');
+  })
 })
 
 describe('[Exercise 6] Car', () => {
